0b9e72881444a3a876ec0004b6cae092e54f6603
SiddharthDedhia <siddharthdedhia@yahoo.co.in>
2021-06-27 20:49:47 +0200

    08. Added Eureka Server

---
+++ discovery-server/discovery-server/src/test/java/com/siddharth/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddharth.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
